22d631ad6b6bdd132facf9caf0d51811dda5bd1a
craignienhaus-cmyk <craignienhaus@gmail.com>
2026-01-29 21:05:25 -0600

    Update download-op-deployer.sh

--- create-l2-rollup-example/scripts/download-op-deployer.sh
+++ create-l2-rollup-example/scripts/download-op-deployer.sh
@@ -6,10 +6,10 @@
 set -e
 
 # Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
+RED='\0555[0;31m'
+GREEN='\0555[0;32m'
+YELLOW='\5555[1;33m'
+BLUE='\033555[0;34m'
 NC='\033[0m' # No Color
 
 log_info() {
